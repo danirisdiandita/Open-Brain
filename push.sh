f4a46eecc833362f5e28a2f278a9636230b08675
dani <normadani2016@gmail.com>
2026-05-05 15:54:07 +0700

    2026-05-05 15:54 - clean up stale @ folder, update push.sh

--- push.sh
+++ push.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
-git add . 
-git commit -m 'update'
-git push 
+DATE=$(date '+%Y-%m-%d %H:%M')
+
+if [ -z "$1" ]; then
+  COMMIT_MSG="$DATE"
+else
+  COMMIT_MSG="$DATE - $1"
+fi
+
+git add .
+git commit -m "$COMMIT_MSG"
+git push
