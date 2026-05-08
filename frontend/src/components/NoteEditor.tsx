05d9d0cb8b892cac9cfdfdbb5e2fb32813e33a7a
dani <normadani2016@gmail.com>
2026-05-08 09:38:58 +0700

    2026-05-08 09:38 - add onCreate debug, editorProps for novel

--- frontend/src/components/NoteEditor.tsx
+++ frontend/src/components/NoteEditor.tsx
@@ -38,6 +38,14 @@ export function NoteEditor({ content, onChange }: NoteEditorProps) {
         initialContent={initialContent}
         extensions={extensions}
         onUpdate={handleUpdate}
+        onCreate={({ editor }: { editor: EditorInstance }) => {
+          console.log("editor created", editor)
+        }}
+        editorProps={{
+          attributes: {
+            class: "prose prose-sm max-w-none focus:outline-none min-h-[200px] p-4",
+          },
+        }}
       />
     </EditorRoot>
   )
